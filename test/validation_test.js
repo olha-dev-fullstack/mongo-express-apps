b13564fc1d036d08c62ee88042bbfa03c1ec4461
Olha <o.v.shevel@gmail.com>
2024-12-09 14:41:45 +0200

    add validation to user tests

---
+++ test/validation_test.js
@@ -0,0 +1,20 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Validating records", () => {
+  it("requires a user name", () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+    console.log(message);
+
+    assert(message === "Name is required.");
+  });
+
+  it("requires a users name longer tbat 2 characters", () => {
+    const user = new User({ name: "Al" });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+    assert(message === "Name must be longer than 2 characters");
+  });
+});
