d21e057902b1f7972ea2164bf7d2f8fa50febbdb
Olha <o.v.shevel@gmail.com>
2024-12-10 14:58:18 +0200

    add middleware test

--- src/user.js
+++ src/user.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const PostSchema = require("./post");
+const mongoose = require('mongoose');
+const PostSchema = require('./post');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -7,24 +7,31 @@ const UserSchema = new Schema({
     type: String,
     validate: {
       validator: (name) => name.length > 2,
-      message: "Name must be longer than 2 characters.",
+      message: 'Name must be longer than 2 characters.',
     },
-    required: [true, "Name is required."],
+    required: [true, 'Name is required.'],
   },
   posts: [PostSchema],
   likes: Number,
   blogPosts: [
     {
       type: Schema.Types.ObjectId,
-      ref: "blogPost",
+      ref: 'blogPost',
     },
   ],
 });
 
-UserSchema.virtual("postCount").get(function () {
+UserSchema.virtual('postCount').get(function () {
   return this.posts.length;
 });
 
-const User = mongoose.model("user", UserSchema);
+UserSchema.pre('deleteOne', { document: true }, function (next) {
+  const BlogPost = mongoose.model('blogPost');
+  // this === joe
+
+  BlogPost.deleteMany({ _id: { $in: this.blogPosts } }).then(() => next());
+});
+
+const User = mongoose.model('user', UserSchema);
 
 module.exports = User;
