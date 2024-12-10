10e42f606c1738a20fb044794dd87301b45d4349
Olha <o.v.shevel@gmail.com>
2024-12-10 17:42:39 +0200

    add application files

---
+++ up-star-music-app/database/models/album.js
@@ -0,0 +1,13 @@
+const mongoose = reauire("mongoose");
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: String,
+  data: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number,
+});
+
+module.exports = AlbumSchema;
